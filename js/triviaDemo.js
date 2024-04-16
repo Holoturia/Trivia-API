2e4e145c270b6c709d493089c5f544f732b393ca
vilamujordi <vilamujordijr@gmail.com>
2024-04-16 10:56:59 +0200

    Trivia game done v1

--- js/triviaDemo.js
+++ js/triviaDemo.js
@@ -1,4 +1,5 @@
 const APILink = "https://opentdb.com/api.php?amount=1&type=multiple";
+let correctAnswer = "";
 
 function getQuestion(){
     let category, difficulty;
@@ -6,7 +7,12 @@ function getQuestion(){
     category = document.getElementById("category").value;
     difficulty = document.getElementById("difficulty").value;
 
-    printData(fetchQuestion(category, difficulty));
+    try{
+        fetchQuestion(category, difficulty);
+    }
+    catch (TypeError){
+        document.getElementById("output").innerHTML = `<p>No data<p>`;
+    }
 }
 
 function fetchQuestion(category, difficulty){
@@ -20,15 +26,56 @@ function fetchQuestion(category, difficulty){
         link += "&difficulty=" + difficulty;
     }
 
-    fetch(link)
-    .then(function getResult(result){
-        return result.json();
-    })
-    .then(function getData(data){
-        return data;
-    })
+    try{
+        fetch(link)
+        .then(function getResult(result){
+            return result.json();
+        })
+        .then(function getData(data){
+            console.log(data);
+
+            correctAnswer = data.results[0].correct_answer;
+
+            printData(data);
+        })
+    }
+    catch{
+
+    }
+    
 }
 
 function printData(data){
-    document.getElementById("output") = data.results[0].question;
+    let answers = [];
+    answers.push(data.results[0].correct_answer);
+
+    data.results[0].incorrect_answers.forEach(element => {
+        answers.push(element);
+    });
+
+    answers.sort(() => Math.random() - 0.5);    //Shuffle
+
+    document.getElementById("output").innerHTML = `
+    <p>${data.results[0].question}<p>
+    <input type="radio" id="q1" name="answer" value="${answers[0]}">
+    <label for="q1">${answers[0]}</label><br>
+    <input type="radio" id="q2" name="answer" value="${answers[1]}">
+    <label for="q2">${answers[1]}</label><br>
+    <input type="radio" id="q3" name="answer" value="${answers[2]}">
+    <label for="q3">${answers[2]}</label><br>
+    <input type="radio" id="q4" name="answer" value="${answers[3]}">
+    <label for="q4">${answers[3]}</label><br><br>
+    <button onclick="checkResult()">Check result</button>
+    `;
+}
+
+function checkResult(){
+    let playerAnswer = document.querySelector("input[type='radio'][name=answer]:checked").value;
+
+    if (playerAnswer == correctAnswer){
+        alert("Correct!")
+    }
+    else{
+        alert("Incorrect!");
+    }
 }
